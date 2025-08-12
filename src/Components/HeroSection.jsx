da0c49b4811a37428e989c5c43ebd47e11a685cf
ghazi <zakariaghazi77@gmail.com>
2025-08-12 16:59:28 +0100

    adding swiper to the hero section

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,31 +1,61 @@
-
-import { useState ,useEffect } from "react";
-import axios from "axios";
+import { useState, useEffect } from "react";
+import { api } from "./api";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination , Autoplay } from "swiper/modules";
+import { Pagination, Autoplay } from "swiper/modules";
+import "../Styles/HeroSection.css";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
 
 export const HeroSection = () => {
-
-    useEffect(() => {
-        axios.get("")
-    },[])
+  const [data, setDate] = useState([]);
+  useEffect(() => {
+    api
+      .get(`/popular?api_key=${import.meta.env.VITE_API_KEY}`)
+      .then((res) => {
+        setDate(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <>
-      <Swiper
-        modules={[Pagination, Autoplay]}
-        pagination={{
-          dynamicBullets: true,
-        }}
-        loop={true}
-        autoplay={{delay: 3000}}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-      </Swiper>
+      <section>
+        <div className="hero_container">
+          <Swiper
+            modules={[Pagination, Autoplay]}
+            pagination={{ dynamicBullets: true }}
+            loop={true}
+            // autoplay={{ delay: 3000 }}
+            className="mySwiper"
+          >
+            {data.map((item) => (
+              <SwiperSlide key={item.id} className="slide">
+                <img
+                  src={`https://image.tmdb.org/t/p/original/${item.backdrop_path}`}
+                  alt={item.title}
+                />
+                <div className="overlay" />
+                <div className="content">
+                  <h1>{item.title}</h1>
+                  <p>
+                    <strong>Release Date:</strong> {item.release_date}
+                  </p>
+                  <p>
+                    <strong>Language:</strong>{" "}
+                    {item.original_language.toUpperCase()}
+                  </p>
+                  <p>
+                    <strong>Rating:</strong> {item.vote_average} / 10
+                  </p>
+                  <p className="overview">{item.overview}</p>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </section>
     </>
   );
 };
